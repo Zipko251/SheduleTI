10a06e3640a4d1e76b3bce22f50b240f6b1b9c5a
Sma1ck <maximagapov2000@gmail.com>
2019-10-07 18:04:54 +0300

    add requirements

--- Bot.py
+++ Bot.py
@@ -72,7 +72,7 @@ if "HEROKU" in list(os.environ.keys()):
         bot.remove_webhook()
         bot.set_webhook(url="https://sheduleti.herokuapp.com")
         return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 else:
     # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
     # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
